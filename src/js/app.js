c65c6369c36e11eb964c6d0229c34c0e8d6a854d
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-09-25 00:17:22 +0700

    Готово

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
-/* eslint-disable consistent-return */
-/* eslint-disable array-callback-return */
-/* eslint-disable import/no-cycle */
-/* eslint-disable prefer-destructuring */
-// eslint-disable-next-line no-console
-console.log('it works!');
+const btn = document.querySelector('[toggle="popover"]');
+const popover = document.querySelector('[widget="popover-top"]');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  popover.classList.toggle('active');
+  popover.classList.toggle('invisible');
+});
