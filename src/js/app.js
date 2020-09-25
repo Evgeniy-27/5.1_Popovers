9313887f0c79b80996e220678acba57e0511124f
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-09-25 23:34:27 +0700

    Корректировки

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-undef */
+import Widget from './Widget';
+
+const widget = new Widget();
+const container = document.querySelector('.container');
 const btn = document.querySelector('[toggle="popover"]');
-const popover = document.querySelector('[widget="popover-top"]');
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
+  container.insertAdjacentHTML('afterbegin', widget.div);
+  const popover = container.querySelector('[widget="popover-top"]');
   popover.classList.toggle('active');
   popover.classList.toggle('invisible');
 });
